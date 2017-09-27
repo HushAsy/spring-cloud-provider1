738cfd0d80af28870b5c03c39d2fcd3f99e92a41
何海水 <270232645@qq.com>
2017-09-27 23:47:04 +0800

    Initial commit

---
+++ src/main/java/org/hhs/SpringCloudProvider1Application.java
@@ -0,0 +1,14 @@
+package org.hhs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SpringCloudProvider1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudProvider1Application.class, args);
+	}
+}
